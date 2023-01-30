6c04358d8e071ed77603dfb180ff2df9c9c90e97
ethanrosner <96458095+ethanrosner@users.noreply.github.com>
2023-01-30 13:19:53 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,8 +1,7 @@
 import pygame
 import math
 import random
-from numpy import *
-
+#from numpy import *
 
 pygame.init()
 
@@ -11,79 +10,69 @@ Y = 1000
 
 screen = pygame.display.set_mode((X, Y))
 pygame.display.set_caption('Show Text')
+display_surface = pygame.display.set_mode((X, Y))
+
+titleFont = pygame.font.Font('freesansbold.ttf', 32)
+wordFont = pygame.font.Font('freesansbold.ttf', 50)
+loseFont = pygame.font.Font('freesansbold.ttf', 100)
+
+# these guys literally watching anime wtf and omri is watching cars -_-
+
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
-# Takes Care of the word bank
 
-wordBank = []
-with open("countryBank.txt", 'r') as f:
-    fileData = f.readlines()
-    for line in fileData:
-        wordBank.append(line.replace('\n', ''))
-
-# Word bank is successfully filled.
-
-display_surface = pygame.display.set_mode((X, Y))
-titleFont = pygame.font.Font('freesansbold.ttf', 32)
-wordFont = pygame.font.Font('freesansbold.ttf', 50)
-title = titleFont.render("Countries Hangman", True, white)
-titleRect = title.get_rect()
-titleRect.center = (X * 0.5, Y * 0.2)
-
-
-# def getWord():
-rSlotN = random.randint(0, 193)
-genWord = wordBank[rSlotN]
-# genWordL = len(genWord)
-# print(genWordL)
-
-genWordReplaced = ""
-
-print(genWord)
-
-for char in genWord:
-    if char != " " and char != "-":
-        genWordReplaced += "_ "
-    elif char == " ":
-        genWordReplaced += "  "
-    elif char == "-":
-        genWordReplaced += "-"
-print(genWordReplaced)
-
-genUnderWordShow = wordFont.render(genWordReplaced, True, white)
-genUnderRect = genUnderWordShow.get_rect()
-genUnderRect.center = (X * 0.5, Y * 0.8)
-
-genWordShow = wordFont.render(genWordReplaced, True, white)
-genRect = genWordShow.get_rect()
-genRect.center = (X * 0.5, Y * 0.79)
-
-# array of word to be guessed
-wordToGuess = array([[" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y],
-                     [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y], [" ", black, X, Y]], dtype=object)
-
-# enter letters and locations to array
-genLength = len(genWord)
-print(genLength)
-
-for x in range(genLength):
-    wordToGuess[x,0] = " "
-
-
-# def playGame():
-
-
-while True:
+def displayTitle():
+    title = titleFont.render("Countries Hangman", True, white)
+    titleRect = title.get_rect()
+    titleRect.center = (X * 0.5, Y * 0.2)
     display_surface.blit(title, titleRect)
+
+
+
+
+def getWord():
+    global wrongGuessCounter
+    wrongGuessCounter = 9
+    global gameWord
+    wordBank = []
+    with open("countryBank.txt", 'r') as f:
+        fileData = f.readlines()
+        for line in fileData:
+            wordBank.append(line.replace('\n', ''))
+    genWord = "Saint Vincent and the Grenadines" # wordBank[random.randrange(0, 193)]
+    if len(genWord) > 15:
+        enterNeed = True
+    tempWord, gameWord = "", ""
+
+    for char in genWord:
+        tempWordLen = len(tempWord)
+        if enterNeed == True and char == " " and tempWordLen >=30:
+            tempWord += "\n"
+            gameWord += "\n"
+            enterNeed = False
+        elif char == " " or char == "-":
+            tempWord += char + " "
+            gameWord += char + " "
+        else:
+            tempWord += char + " "
+            gameWord += "_ "
+
+
+        if char == " " or char == "-":
+            gameWord += char + " "
+        else:
+            gameWord += "_ "
+
+    tempWord, gameWord = tempWord[:-1], gameWord[:-1]
+    print (tempWord)
+    print(gameWord)
+
+
+def displayGameSetup():
+    global wrongGuessCounter
     # pregame hangman
     # base horizontal line
     pygame.draw.line(screen, white, (X * 0.3, Y * 0.6), (X * 0.5, Y * 0.6), width=3)
@@ -94,31 +83,46 @@ while True:
     # head holder line
     pygame.draw.line(screen, white, (X * 0.55, Y * 0.3), (X * 0.55, Y * 0.25), width=3)
 
-    # checking for word
+    if wrongGuessCounter >= 1:
+        pygame.draw.circle(screen, white, (X * 0.55, Y * 0.335), (X * 0.035), width=3) # head
+    if wrongGuessCounter >= 2:
+        pygame.draw.line(screen, white, (X * 0.55, Y * 0.368), (X * 0.55, Y * 0.5), width=3) # body
+    if wrongGuessCounter >= 3:
+        pygame.draw.line(screen, white, (X * 0.55, Y * 0.5), (X * 0.5, Y * 0.55), width=3) # left leg
+    if wrongGuessCounter >= 4:
+        pygame.draw.line(screen, white, (X * 0.55, Y * 0.5), (X * 0.6, Y * 0.55), width=3) # right leg
+    if wrongGuessCounter >= 5:
+        pygame.draw.line(screen, white, (X * 0.55, Y * 0.4), (X * 0.5, Y * 0.43), width=3) # left hand
+    if wrongGuessCounter >= 6:
+        pygame.draw.line(screen, white, (X * 0.55, Y * 0.4), (X * 0.6, Y * 0.43), width=3) # right hand
+    if wrongGuessCounter >= 7:
+        pygame.draw.circle(screen, white, (X * 0.535, Y * 0.33), (X * 0.005)) # left eye
+    if wrongGuessCounter >= 8:
+        pygame.draw.circle(screen, white, (X * 0.565, Y * 0.33), (X * 0.005)) # right eye
+    if wrongGuessCounter >= 9:
+        pygame.draw.arc(screen, white, (X * 0.535, Y * 0.347, X * 0.03, Y * 0.01), 2 * math.pi, 1 * math.pi) # mouth
+        loseText = loseFont.render("YOU LOST", True, red)
+        lostRect = loseText.get_rect()
+        lostRect.center = (X * 0.5, Y * 0.5)
+        display_surface.blit(loseText, lostRect)
 
-    # drawing word text
-    display_surface.blit(genUnderWordShow, genUnderRect)
-    display_surface.blit(genWordShow, genRect)
+def displayWord():
+    global gameWord
+    gameWordShow = wordFont.render(gameWord, True, white)
+    gameWordRect = gameWordShow.get_rect()
+    gameWordRect.center = (X * 0.5, Y * 0.8)
+    display_surface.blit(gameWordShow, gameWordRect)
 
-    # in game drawing
-    # head
-    pygame.draw.circle(screen, white, (X * 0.55, Y * 0.335), (X * 0.035), width=3)
-    # body
-    pygame.draw.line(screen, white, (X * 0.55, Y * 0.368), (X * 0.55, Y * 0.5), width=3)
-    # left leg
-    pygame.draw.line(screen, white, (X * 0.55, Y * 0.5), (X * 0.5, Y * 0.55), width=3)
-    # right leg
-    pygame.draw.line(screen, white, (X * 0.55, Y * 0.5), (X * 0.6, Y * 0.55), width=3)
-    # left hand
-    pygame.draw.line(screen, white, (X * 0.55, Y * 0.4), (X * 0.5, Y * 0.43), width=3)
-    # right hand
-    pygame.draw.line(screen, white, (X * 0.55, Y * 0.4), (X * 0.6, Y * 0.43), width=3)
-    # left eye
-    pygame.draw.circle(screen, white, (X * 0.535, Y * 0.33), (X * 0.005))
-    # right eye
-    pygame.draw.circle(screen, white, (X * 0.565, Y * 0.33), (X * 0.005))
-    # mouth
-    pygame.draw.arc(screen, white, (X * 0.535, Y * 0.347, X * 0.03, Y * 0.01), 2 * math.pi, 1 * math.pi)
+
+
+getWord()
+
+
+while True:
+    displayTitle()
+    displayGameSetup()
+    displayWord()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
